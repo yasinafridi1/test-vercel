c429bf263e28cae8db5a64702339f8dba96f5ed2
yasinafridi1 <yaseenafridi10875@gmail.com>
2024-11-14 02:53:38 +0500

    test

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT;
+
+app.get("/health", (req, res) => {
+  return res.status(200).json({ message: "Hello world" });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
