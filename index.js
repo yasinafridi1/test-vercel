5de31b1e3265d1cda27d7cee7b1b90540bb05470
yasinafridi1 <yaseenafridi10875@gmail.com>
2024-11-14 02:59:09 +0500

    json changed

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 4000;
 
 app.get("/health", (req, res) => {
   return res.status(200).json({ message: "Hello world" });
